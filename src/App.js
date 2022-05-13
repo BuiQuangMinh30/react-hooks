f7d55bad703bc3afd027907979313111a86f611b
bqminh <64219602+BuiQuangMinh30@users.noreply.github.com>
2022-05-13 15:55:35 +0700

    twoway binding

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
 import { useState } from "react";
-const orders = [100,200,300,500,600,700]
+const gifts= [
+  '123',
+  'mot hai ba',
+  'mot 2 ba'
+]
+const courses = [
+  {
+    id: 1,
+    name: 'HTML, CSS'
+  }, 
+  {
+    id: 2,
+    name: 'HTML, CSS, JS'
+  }
+]
 function App() {
-  // const [counter, setCounter] = useState(1);//giá trị khởi tạo là 1
-  const [counter, setCounter] = useState(()=>{
-    const total = orders.reduce((total,cur)=> total+cur)
-    return total
-  });//giá trị khởi tạo là 1
-  const handleIncrease = () => {
-    //setCounter(counter + 1);
-    //setCounter(counter + 1);
-    
-    //khi truyền prevState thì có thể callbacks. GỘp 3 lại thực thi 1 lần
-    // setCounter(prevState => prevState +1)
-    setCounter(prevState => prevState +1)
-   
-    // sau khi gọi setCounter thì react sẽ gọi hàm App. Lại đọc code từ
-    //trên xuống. 
-  };
-  console.log('re-render')
+ //const [gift, setGift] = useState();
+//  const [name, setName] = useState();
+//  const [emai, setEmail] = useState();
+// const [checked, setChecked] = useState();
+const [checked, setChecked] = useState([
+
+]);
+
+  // const randomGift = () =>{
+  //   const index = Math.floor(Math.random() *gifts.length)
+
+  //   setGift(gifts[index])
+  // }
+  //console.log(checked)
+  const handleSubmit = () => {
+    //Call API 
+    console.log({id: checked})
+  }
+  const handldeCheck = (id) =>{
+    setChecked(prev =>  {
+      const isChecked = checked.includes(id)
+      if(isChecked){
+        //Uncheck
+        return checked.filter(item => item !== id)//lọc lấy ra cái th khác id check
+      }
+      else{
+        return [...prev,id]
+      }
+    })
+  }
   return (
     <div>
-      <h1>{counter}</h1>
-      
-      <button onClick={handleIncrease}>Increase</button>
+      {/* <h1>{gift || 'Chưa có phần thưởng'}</h1>
+      <button onClick={randomGift}> Lấy thưởng</button> */}
+      {/* // <input onChange={e => setName(e.target.value)} value={name}/>
+      // <input onChange={e => setEmail(e.target.value)} value={emai}/> */}
+      {courses.map(course => (
+        <>
+        {/* radio */}
+          {/* <div key={course.id}>
+            <input type="radio"
+            onChange={()=> setChecked(course.id)}
+            checked={checked === course.id}/>
+            {course.name}
+          </div> */}
+
+          <div key={course.id}>
+            <input type="checkbox"
+            onChange={() => handldeCheck(course.id)}
+            checked={checked.includes(course.id)}/>
+            {course.name}
+            
+          </div>
+        </>
+      ))}
+      <button onClick={handleSubmit}>Change</button>
     </div>
   );
 }
